99ebd8b142db774278cef97058e191d3ff44c4e3
miyatamiho <t.td.k45@gmail.com>
2023-02-14 02:32:48 +0000

    エラー発生状態

--- resources/views/admin/profile/create.blade.php
+++ resources/views/admin/profile/create.blade.php
@@ -1,10 +1,46 @@
     @extends('layouts.profile')
     @section('title', 'プロフィールの新規作成')
+    
     @section('content')
         <div class="container">
             <div class="row">
                 <div class="col-md-8 mx-auto">
                     <h3>プロフィール新規作成</h3>
+                    <form action={{route('admin.profile.create') }} method="post" enctype="multipart/form-data">
+                        
+                    @if (count($errors) > 0)
+                        <ul>
+                            @foreach($errors->all() as $e)
+                                <li>{{ $e }}</li>
+                                @endforeach
+                        </ul>
+                    @endif
+                    <div class="form-group row">
+                        <label class="col-md-2">氏名</label>
+                        <div class="col-md-10">
+                            <input type="text" class="form-control" name="name" value="{{ old('name') }}">
+                        </div>
+                    </div>
+                        <div class="form-group row">
+                            <label class="col-md-2">性別</label>
+                            <div class="col-md-10">
+                                <input type="radio" name="gender" id="male" value="male" {{ old('gender') == 'male' ? 'checked' : '' }}>
+                                <label for="male">男性</label>
+                            </div>
+                            <div>
+                                <input type="radio" name="gender" id="female" value="female" {{ old('gender') == 'female' ? 'checked' : '' }}>
+                                <label for="female">女性</label>
+                            </div>
+                        </div>
+                            <div class="form-group row">
+                                <label class="col-md-2">自己紹介欄</label>
+                                <div class="col-md-10">
+                                    <textarea class="form-control" name="introduction" rows="20">{{ old('introduction') }}</textarea>
+                                </div>
+                            </div>
+                            @csrf
+                            <input type="submit" class="btn btn-primary" value="送信">
+                    </form>
                 </div>
             </div>
         </div>
--- routes/web.php
+++ routes/web.php
@@ -19,15 +19,17 @@ Route::get('/', function () {
 
 
 use App\Http\Controllers\Admin\NewsController;
-Route::controller(NewsController::class)->prefix('admin')->group(function() {
-    Route::get('news/create', 'add')->middleware('auth');
+Route::controller(NewsController::class)->prefix('admin')->name('admin.')->middleware('auth')->group(function() {
+    Route::get('news/create', 'add')->name('news.add');
+    Route::post('news/create', 'create')->name('news.create');
 });
     
 use App\Http\Controllers\Admin\ProfileController;
-Route::controller(ProfileController::class)->prefix('admin')->group(function(){
+Route::controller(ProfileController::class)->prefix('admin')->middleware('auth')->group(function(){
+    Route::post('profile/create', 'ProfileController@create')->name('profile.create');
+    Route::post('profile/edit', 'ProfileController@update')->name('profile.update');
     Route::get('profile/create', 'ProfileController@add')->middleware('auth');
     Route::get('profile/edit', 'ProfileController@add')->middleware('auth');
-    
 });
 
 
