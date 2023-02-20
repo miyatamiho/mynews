77044287b2fae4a2af272721781cb6246971f9ba
miyatamiho <t.td.k45@gmail.com>
2023-02-20 04:11:19 +0000

    更新削除の課題

--- database/migrations/2014_10_12_000000_create_users_table.php
+++ database/migrations/2014_10_12_000000_create_users_table.php
@@ -34,3 +34,13 @@ return new class extends Migration
         Schema::dropIfExists('users');
     }
 };
+
+User::create([
+            'name' => '森本',
+            'email' => 'morimoto@tech.com',
+            'password' => Hash::make("pass"),
+       ]);
+
+
+
+
--- resources/views/admin/profile/edit.blade.php
+++ resources/views/admin/profile/edit.blade.php
@@ -1,13 +1,56 @@
-<!DOCTYPE html>
-<html>
-    <head>
-        <meta charset="utf-8">
-        <meta http-equiv="X-UA-Compatible" content="IE=edge">
-        <meta name="viewport" content="width=device-width, initial-scale=1">
+@extends('layouts.profile')
+@section('title', 'プロフィールの編集')
 
-        <title>ProfileEdit</title>
-    </head>
-    <body>
-        <h1>プロフィール編集画面</h1>
-    </body>
-</html>
+@section('content')
+    <div class="container">
+        <div class="row">
+            <div class="col-md-8 mx-auto">
+                <h3>プロフィール編集</h3>
+                <form action="{{ route('admin.profile.update') }}" method="post" enctype="multipart/form-data">
+                     @if (count($errors) > 0)
+                        <ul>
+                            @foreach($errors->all() as $e)
+                                <li>{{ $e }}</li>
+                            @endforeach
+                        </ul>
+                    @endif
+                    <div class="form-group row">
+                        <label class="col-md-2" for="name">氏名</label>
+                        <div class="col-md-10">
+                            <input type="text" class="form-control" name="name" value="name">
+                        </div>
+                    </div>
+                    <div class="form-group row">
+                        <label class="col-md-2" for="gender">性別</label>
+                        <div class="col-md-10">
+                            <label class="radio-inline">
+                                <input type="radio" class="form-control "name="gender" value="male">男性
+                            </label>
+                            <label class="radio-inline">
+                                <input type="radio" class="form-control "name="gender" value="female">女性
+                            </label>
+                        </div>
+                    </div>
+                    <div class="form-group row">
+                            <label class="col-md-2" for="hobby">趣味</label>
+                        <div class="col-md-10">
+                            <input type="text" class="form-control" name="hobby" value="hobby">
+                        </div>
+                    </div>
+                    <div class="form-group row">
+                        <label class="col-md-2" for="body">自己紹介</label>
+                        <div class="col-md-10">
+                            <textarea class="form-control" name="body" rows="20"></textarea>
+                        </div>
+                    </div>
+                </form>
+            </div>
+             <div class="form-group row">
+                        <div class="col-md-10">
+                            <input type="hidden" name="id" value="id">
+                            @csrf
+                            <input type="submit" class="btn btn-primary" value="更新">
+                        </div>
+                    </div>
+        </div>
+    </div>
